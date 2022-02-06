80ad5633844cf3e5e0f5723a02c52a26b92071cd
mr-siddique <aamirrahman32@gmail.com>
2022-02-06 16:39:54 +0530

    test message

--- one.cpp
+++ one.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello world";
+    cout<<"hello world"<<"\n";
+    int n; cin>>n;
+    cout<<n%2<<"\n";
     for (int i=1;i<=10;i++) cout<<i+i-1<<"\n";aa
     return 0
 }
